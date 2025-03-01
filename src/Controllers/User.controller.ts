12d12ac109e315e915e7ddc485e6251ac2013cf9
lucia nambi <nambilucia@gmail.com>
2025-03-01 15:50:15 +0300

    fic route error

--- src/Controllers/User.controller.ts
+++ src/Controllers/User.controller.ts
@@ -1,27 +1,27 @@
 import { Request, Response } from "express";
 import { createUser, getUserByEmail } from "../Services/User.service";
 
-export const register = async (req: Request, res: Response): Promise<any> => {
+export const register = async (req: Request, res: Response): Promise<void> => {
   try {
     const { fullname, email, password } = req.body;
 
     if (!fullname || !email || !password) {
-      return res.status(400).json({ message: "All fields are required" });
+      res.status(400).json({ message: "All fields are required" });
     }
 
     // Check if user already exists
     const existingUser = await getUserByEmail(email);
 
     if (existingUser) {
-      return res.status(400).json({ message: "User already exists" });
+     res.status(400).json({ message: "User already exists" });
     }
 
     // Create new user
     const user = await createUser(fullname, email, password);
 
-    return res.status(201).json({ message: "User registered successfully", user });
+     res.status(201).json({ message: "User registered successfully", user });
   } catch (error) {
     console.error("Register error:", error);
-    return res.status(500).json({ message: "Internal Server Error" });
+     res.status(500).json({ message: "Internal Server Error" });
   }
 };
