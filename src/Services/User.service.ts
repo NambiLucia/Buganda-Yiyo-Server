88b2da1bf841f9c0d9a4562c9c837e7f2fa3c092
lucia nambi <nambilucia@gmail.com>
2025-03-04 07:40:09 +0300

    create login user service

--- src/Services/User.service.ts
+++ src/Services/User.service.ts
@@ -1,6 +1,9 @@
 import prisma from "../prisma";
 import bcrypt from "bcrypt";
 import { User } from "@prisma/client";
+import jwt from 'jsonwebtoken'
+import dotenv from "dotenv"
+dotenv.config()
 
 export const createUser = async (fullname: string, email: string, password: string): Promise<User> => {
   const hashedPassword = await bcrypt.hash(password, 10);
@@ -19,3 +22,21 @@ export const getUserByEmail = async (email: string): Promise<User | null> => {
     where: { email },
   });
 };
+
+
+export const loginUser =async( email: string, password: string)=>{
+const user =await getUserByEmail(email)
+if(!user){
+  return null
+
+}
+const matchPaswword =await bcrypt.compare(password,user.password)
+if(!matchPaswword){
+  return null
+ 
+}
+
+return user;
+
+
+}
